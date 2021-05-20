ea02983297252cc31fdaa884c990cf2e2bc26e10
dw1a <dw1a@DW1A-11>
2021-05-20 19:33:03 +0200

    primero

---
+++ prueba2/src/prueba2/prueba2.java
@@ -0,0 +1,19 @@
+package prueba2;
+
+public abstract class prueba2 {
+ private float area, perimetro;
+ 
+	public float getArea() {
+		return area;
+	}
+	public void setArea(float area) {
+		this.area = area;
+	}
+	public float getPerimetro() {
+		return perimetro;
+	}
+	public void setPerimetro(float perimetro) {
+		this.perimetro = perimetro;
+	}
+ 
+}
